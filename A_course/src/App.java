f37c9d3ccae03638ba2d55c4512797449065787d
Danya <dan.shell@bk.ru>
2024-04-10 13:47:49 +0300

    types of data

--- A_course/src/App.java
+++ A_course/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("test");
+        byte num = 100; // -128..127
+        short num2=200; // -32768..32767
+        int num3 = 333;// -2147483648..2147483647
+        long num4 = 21492349; // -9223372036854775808..9223372036854775807
+        float num5=3.14f ; // 1.4E-45..3.4028235E38
+        double num6=3.1415926535897932384626433832795;
+        char ch = 'a'; // 0..65535
+        boolean isFalse= true;
+
+    }
+    public static void test(){
+        System.out.println("test");
     }
 }
