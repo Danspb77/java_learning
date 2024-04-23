03ff2727471ca441d5331a22fff8e976c62df306
Danya <dan.shell@bk.ru>
2024-04-23 17:47:39 +0300

    add alerts

--- selenium_learning/mav/demo/src/main/java/com/drag_and_drop/start.java
+++ selenium_learning/mav/demo/src/main/java/com/drag_and_drop/start.java
@@ -1,7 +1,9 @@
 package com.drag_and_drop;
 
 import java.time.Duration;
+import java.util.function.Function;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,6 +15,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class start {
+    /**
+     * @param args
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         WebDriver driver= new ChromeDriver();
@@ -27,7 +33,33 @@ public class start {
             Actions action= new Actions(driver);
             // it will move to the element "drag" , catch it and hold it , move to the element "drop" , release it
             action.moveToElement(drag).clickAndHold().moveToElement(drop).release().build().perform();
-        
+            // same thing action.dragAndDrop(drag, drop).build().perform();
+            
+            // alerts
+            WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(5));
+            
+            WebElement link1=driver.findElement(By.id("a"));
+            WebElement link2=driver.findElement(By.id("b"));
+            WebElement link3=driver.findElement(By.id("c"));
+
+            link1.click();
+            
+            Alert alert=wait.until(alertIsPresent());
+            alert.accept();
+
+
+            link2.click();
+            Alert prompt= wait.until(alertIsPresent());
+            prompt.sendKeys("Hi");
+            prompt.accept();
+
+            link3.click();
+            Alert dismiss=wait.until(alertIsPresent());
+            dismiss.dismiss();
+
+
+
+
         }
         catch (Exception e) {
             System.out.println(e);
@@ -43,5 +75,10 @@ public class start {
 
 
 
+    }
+
+    private static Function<? super WebDriver, Alert> alertIsPresent() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'alertIsPresent'");
     }
 }
---
+++ selenium_learning/mav/demo/src/main/java/com/example/alerts/Start.java
@@ -0,0 +1,5 @@
+package com.example.alerts;
+
+public class Start {
+    
+}
