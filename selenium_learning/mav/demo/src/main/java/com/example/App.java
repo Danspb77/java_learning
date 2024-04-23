316e99845695548d81ef7c6116880181843f619c
Danya <dan.shell@bk.ru>
2024-04-23 12:38:33 +0300

    add selectors

--- selenium_learning/mav/demo/src/main/java/com/example/App.java
+++ selenium_learning/mav/demo/src/main/java/com/example/App.java
@@ -1,5 +1,7 @@
 package com.example;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
@@ -14,6 +16,8 @@ public class App
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver") ;
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
+        WebElement input = driver.findElement(By.xpath("//*[@id='APjFqb']"));
+        WebElement logo = driver.findElement(By.cssSelector("body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img "));
 
     }
 }
