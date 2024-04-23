35b186d0d649a45bf7b63026a4982f7eda704501
Danya <dan.shell@bk.ru>
2024-04-23 13:35:21 +0300

    add css selectors theory

--- selenium_learning/mav/demo/src/main/java/com/example/App.java
+++ selenium_learning/mav/demo/src/main/java/com/example/App.java
@@ -19,5 +19,43 @@ public class App
         WebElement input = driver.findElement(By.xpath("//*[@id='APjFqb']"));
         WebElement logo = driver.findElement(By.cssSelector("body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img "));
 
+        // Xpath
+        // "//div[@class='name of class']"
+        // "// in the beginning of the path means only single element"
+        // "//a[@class='g'][last()]"
+        // "a[text()="search in google"]"
+        // "*[text()="search in google"]"
+        // "span[contains(text(),'search in google')]"
+        // h3[contains(@class,'name of class')]
+
+        // inner classes
+        // //div[@class='name of class'][3]//a[text()='search in google']
+        // "//span[text()="some"]/parent::div/parent::div/following-sibling::div[@class='name of class']"
+
+        // CSS selectors
+        //$$() in console
+        // "#id"
+        // ".class_name"
+        //"[attribute_name='value']"
+        //"[role*='contain this words']"
+        //"[role^='search the beginning']"
+        //"[role$='search at the ending']"
+        // ".class_name1.class_name2[attribute_name='value']"
+        // "div:not(.col)"
+        // "a:not(href^'http')"
+
+        //inners
+        // "div[name='name of div'] div.child_name"
+
+
+        // "div[name='name of div'] > div.child_name"
+
+        //"ul li:first-child"
+        // "ul li:nth-child(2)"
+
+        
+
+
+
     }
 }
